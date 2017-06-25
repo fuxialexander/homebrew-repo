cab6441d6a9bbd37ce1a7587025a6d3b4512eb43
fuxialexander <fuxialexander@users.noreply.github.com>
2017-06-26 00:35:41 +0800

    Update calendarcsv.rb

--- calendarcsv.rb
+++ calendarcsv.rb
@@ -2,7 +2,7 @@ class Calendarcsv < Formula
   desc "Export your recent calendar events as csv."
   url "https://codeload.github.com/fuxialexander/calendarcsv/tar.gz/0.1.0"
   version "0.1.0"
-  sha256 "" 
+  sha256 "3b5258df2ea123e835fe9888ccd18e1917c87aa651553d0cfb3f25c257e6577c" 
 
   def install
     system "unset CC; swift build -c release"
