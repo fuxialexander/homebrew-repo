2d060cba79e17429d60bfc0331c29d0c65b20a6d
fuxialexander <fuxialexander@users.noreply.github.com>
2017-06-26 00:32:09 +0800

    Create calendarcsv.rb

---
+++ calendarcsv.rb
@@ -0,0 +1,12 @@
+class Calendarsync < Formula
+  desc "Export your recent calendar events as csv."
+  url "https://github.com/fuxialexandre/calendarcsv/archive/0.1.0.tar.gz"
+  version "0.1.0"
+  sha256 "" 
+
+  def install
+    system "unset CC; swift build -c release"
+    system "mv .build/release/calendarcsv calendarcsv"
+    bin.install "calendarcsv"
+  end
+end
